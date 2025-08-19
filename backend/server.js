76c508f6c40663cf473f9e7322e924ad7ce20c20
RestinGreen <majairober@gmail.com>
2025-08-19 13:08:58 +0300

    added db init with neon serverless pgsql database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { sql } from './config/db.js';
 
 dotenv.config()
 
 const app = express();
 
+async function initDB() {
+    try {
+
+        await sql `CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+            )`;
+
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.log("Error initializing database:", error);
+        process.exit(1);
+    }
+}
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -12,8 +31,11 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+initDB().then(() => {
 
-    console.log('Server is running on port:', PORT);
+    app.listen(PORT, () => {
+        console.log('Server is running on port:', PORT);
+    });
 
-});
+
+})
