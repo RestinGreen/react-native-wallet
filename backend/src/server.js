4ee80a4e9ee670d25749473830b1bc7ec057b99c
RestinGreen <majairober@gmail.com>
2025-08-20 21:50:01 +0300

    added cronjob

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,16 @@ import rateLimiter from './middleware/rateLimiter.js';
 
 import transactionsRoute from "./routes/transactionsRoute.js"
 import { initDB } from './config/db.js';
+import job from "./config/cron.js"
 
 dotenv.config()
 
 const app = express();
+
+
+if (process.env.NODE_ENV === 'production') {
+    job.start();
+}
 
 //middleware
 app.use(rateLimiter)
@@ -15,7 +21,7 @@ app.use(express.json());
 
 
 
-app.get('/', (req, res) => {
+app.get('/api/health', (req, res) => {
     res.send('Welcome to the backend server!');
 });
 
@@ -23,6 +29,9 @@ app.get('/', (req, res) => {
 app.use("/api/transactions", transactionsRoute)
 
 const PORT = process.env.PORT
+app.get('/api/health', (req, res) => {
+    res.json({ status: 'ok' });
+});
 
 initDB().then(() => {
 
