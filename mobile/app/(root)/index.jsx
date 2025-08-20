11219d79fd2fa6a08e5f89d78b925080ee405fe2
RestinGreen <majairober@gmail.com>
2025-08-20 22:47:00 +0300

    created transaction hooks

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -2,24 +2,40 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SignOutButton } from "@/components/SignOutButton"
+import { useTransactions } from '../../hooks/useTransactions'
+import { useEffect } from 'react'
 
 export default function Page() {
-  const { user } = useUser()
+    const { user } = useUser()
 
-  return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
-    </View>
-  )
+    const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(user.id)
+
+    useEffect(() => {
+        loadData()
+    }, [loadData])
+
+    console.log("user id", user.id);
+    
+    console.log("transactions loaded", transactions);
+    console.log("summary loaded", summary);
+
+    return (
+        <View>
+            <SignedIn>
+                <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+                <Text>Income: {summary.income}</Text>
+                <Text>Expenses: {summary.expenses}</Text>
+                <Text>Balance: {summary.balance}</Text>
+                <SignOutButton />
+            </SignedIn>
+            <SignedOut>
+                <Link href="/(auth)/sign-in">
+                    <Text>Sign in</Text>
+                </Link>
+                <Link href="/(auth)/sign-up">
+                    <Text>Sign up</Text>
+                </Link>
+            </SignedOut>
+        </View>
+    )
 }
